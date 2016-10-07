868c37da435fc0b6467b0f17163c7d080a9ddc46
Xinlun Cheng <chengxinlun@gmail.com>
2016-10-08 00:51:25 +0800

    Debugging

--- cont_fit.py
+++ cont_fit.py
@@ -7,11 +7,15 @@ from code.fitting.fitter import lmlsq
 from code.core.dataio.specio import get_spec
 
 
-w, f, e = get_spec("data/calib/pt/772-56660.pkl")
+w, f, e = get_spec("data/calib/pt/776-56660.pkl")
 contm = ContSdss(f[0], w[0],
                  - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])), 0.0, 0.0,
-                 0.0, 0.0, fixed={"x_0_0": True})
+                 0.0, 0.0)
+print(contm)
 fem = Fe2V(0.0, 900.0, 1.0, 0.0, 900.0, 1.0)
 cf = contm + fem
+cf[0].x_0.fixed = True
+print(cf)
+print(cf[0].x_0.fixed)
 res = lmlsq(cf, w, f, e, 100000)
 print(res)
