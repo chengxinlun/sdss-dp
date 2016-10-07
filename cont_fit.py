804095f87bb74d4ec0ef3bd87f87a1d04e4ef803
Xinlun Cheng <chengxinlun@gmail.com>
2016-10-08 01:14:09 +0800

    Debugging

--- cont_fit.py
+++ cont_fit.py
@@ -11,7 +11,8 @@ from code.core.dataio.specio import get_spec
 w, f, e = get_spec("data/calib/pt/776-56660.pkl")
 fem = Fe2V(0.0, 900.0, 1.0, 0.0, 900.0, 1.0)
 cf = models.PowerLaw1D(f[0], w[0],
-                       - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0]))) + \
-    models.Polynomial1D(3) + fem
+                       - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])),
+                       fixed={'x_0': True}) + models.Polynomial1D(3) + fem
+print(cf.parameters)
 res = lmlsq(cf, w, f, e, 100000)
 print(res)
