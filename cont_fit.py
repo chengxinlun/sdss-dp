7e9609446510a53f2616c2f6201f5b2f91f1d14c
chengxinlun <chengxinlun@gmail.com>
2016-10-08 01:06:20 +0800

    Debugging

--- cont_fit.py
+++ cont_fit.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-from code.fitting.cont import ContSdss
+# from code.fitting.cont import ContSdss
+from astropy.modeling import models
 from code.fitting.fe2 import Fe2V
 from code.fitting.fitter import lmlsq
 from code.core.dataio.specio import get_spec
 
 
 w, f, e = get_spec("data/calib/pt/776-56660.pkl")
-contm = ContSdss(f[0], w[0],
-                 - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])), 0.0, 0.0,
-                 0.0, 0.0)
-print(contm)
 fem = Fe2V(0.0, 900.0, 1.0, 0.0, 900.0, 1.0)
-cf = contm + fem
-cf[0].x_0.fixed = True
-print(cf)
-print(cf[0].x_0.fixed)
+cf = models.PowerLaw1D(f[0], w[0],
+                       - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0]))) + \
+    models.Polynomial1D(3) + fem
 res = lmlsq(cf, w, f, e, 100000)
 print(res)
