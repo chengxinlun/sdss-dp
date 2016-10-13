1c2737b73e9e2323d9881dab881c8b21db973d6f
chengxinlun <chengxinlun@gmail.com>
2016-10-13 10:33:15 +0800

    1. Modofied continuum fitting
    2. Deleted unused debugging

--- cont_fit.py
+++ cont_fit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-# from code.fitting.cont import ContSdss
-from astropy.modeling import models
+from code.fitting.cont import ContSdss
 from code.calib.splitspec import splitspec
 from code.fitting.fe2 import Fe2V
 from code.fitting.fitter import lmlsq
@@ -14,9 +13,8 @@ if __name__ == "__main__":
     wavesplit = [[4200.0, 4300.0], [4450.0, 4750.0], [5100.0, 5500.0]]
     wf, ff, ef = splitspec(w, f, e, wavesplit)
     fem = Fe2V(0.0, 900.0, 1.0, 0.0, 900.0, 1.0)
-    cf = models.PowerLaw1D(f[0], w[0],
-                           - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])),
-                           fixed={'x_0': True}) + models.Polynomial1D(3) + fem
-    print(cf.parameters)
+    cf = ContSdss(ff[0], wf[0],
+                  - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])), 0.0, 0.0,
+                  0.0, 0.0, fixed={'x_0': True}) + fem
     res = lmlsq(cf, wf, ff, ef, 100000)
     print(res.parameters)
