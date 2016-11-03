ea4a140055c8d5f7525154b117d3c8c361d51d9e
chengxinlun <chengxinlun@gmail.com>
2016-11-03 09:08:36 +0800

    Finished spectra fitting for debugging

--- spectra_fit.py
+++ spectra_fit.py
@@ -7,9 +7,13 @@ from code.fitting.fe2 import Fe2V
 from code.fitting.hbeta import Hbeta2
 from code.fitting.narrow import Narrow
 from code.fitting.fitter import lmlsq
+from code.core.dataio.specio import get_spec
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
-def cffit(rmid, mjd, w, f, e):
+def cffit(w, f, e):
     wavesplit = [[4200.0, 4300.0], [4450.0, 4750.0], [5100.0, 5500.0]]
     wf, ff, ef = splitspec(w, f, e, wavesplit)
     fem = Fe2V(0.0, 900.0, 1.0, 0.0, 900.0, 1.0)
@@ -20,7 +24,7 @@ def cffit(rmid, mjd, w, f, e):
     return res.parameters
 
 
-def hofit(rmid, mjd, w, f, e, cfres):
+def hofit(w, f, e, cfres):
     cf = ContSdss(*cfres[:7]) + Fe2V(*cfres[6:-1])
     ff = f - cf(w)
     hbeta = Hbeta2(10.0, 0.0, 500.0, 40.0, 0.0, 1800.0, 40.0, 0.0, 1800.0,
@@ -35,3 +39,20 @@ def hofit(rmid, mjd, w, f, e, cfres):
             'a': [0.0, 50.0], 's': [-1000.0, 1000.0], 'w': [0.0, 1200.0]})
     hdelta = Narrow(10.0, 0.0, 1500.0, 4102.73, fixed={'c': True}, bounds={
         'a': [0.0, 50.0], 's': [-1000.0, 1000.0], 'w': [1200.0, 20000.0]})
+    hgamma = Narrow(20.0, 0.0, 500.0, 4346.42, fixed={'c': True}, bounds={
+        'a': [0.0, 50.0], 's': [-1000.0, 1000.0], 'w': [1200.0, 20000.0]})
+    all_lines = hbeta + o3 + hdelta + hgamma
+    res = lmlsq(all_lines, w, ff, e, 100000)
+    return res.parameters
+
+
+if __name__ == "__main__":
+    w, f, e = get_spec("data/calib/pt/782-56660.pkl")
+    cont_res = cffit(w, f, e)
+    line_res = hofit(w, f, e, cont_res)
+    print(cont_res.parameters)
+    print(line_res.parameters)
+    plt.plot(w, f)
+    plt.plot(w, cont_res(w))
+    plt.plot(w, line_res(w))
+    plt.savefig("782-56660.png")
