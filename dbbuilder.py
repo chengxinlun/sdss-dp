8460d9a65be48eb13141288a1b07988652701d67
chengxinlun <chengxinlun@gmail.com>
2016-12-01 11:26:27 +0800

    Exclude error == 0 points from calibration

--- dbbuilder.py
+++ dbbuilder.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import code.core.dataio.specio as s
-import code.calib.ptcalib as ec
+import code.calib.errorcalib as ec
+import code.calib.ptcalib as pc
 import code.visual.plotspec as p
 from code.core.dataio.rawio import get_rm_list, get_source_info
 
 
 def calib_and_out(rmid, mjd):
-    w, f, e = ec.ptcalib(rmid, mjd)
+    w, f, e = pc.ptcalib(rmid, mjd)
+    w, f, e = ec.errorcalib(w, f, e)
     fn = str(rmid) + "-" + str(mjd) + ".pkl"
     s.save_spec(w, f, e, "data/calib/pt", fn)
     fd = "plot/spectra/" + str(rmid)
