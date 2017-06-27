245a572d3f72d8b8396a3280af86756784136d3d
chengxinlun <chengxinlun@gmail.com>
2017-06-27 12:48:56 +0200

    Using lmfit rather than astropy

--- spectra_fit.py
+++ spectra_fit.py
@@ -5,88 +5,56 @@ import logging
 import logging.config
 import warnings
 import pickle
-import numpy as np
-from code.fitting.cont import ContSdss
+import matplotlib.pyplot as plt
+from lmfit import report_fit
 from code.calib.splitspec import splitspec
-from code.fitting.fe2 import Fe2V
-from code.fitting.hbeta import Hbeta2
-from code.fitting.narrow import Narrow
-from code.fitting.fitter import lmlsq
+from code.fitting.LmCF import LmCF
+from code.fitting.LmHO import LmHO
+from code.fitting.fitter import conggrad
 from code.core.dataio.specio import get_spec
 from code.core.location import Location
 from code.core.util.io import create_directory
-from code.core.util.parallel import para_return
+# from code.core.util.parallel import para_return
 
 
 def cffit(w, f, e, initial):
     wavesplit = [[4200.0, 4300.0], [4450.0, 4750.0], [5100.0, 5500.0]]
     wf, ff, ef = splitspec(w, f, e, wavesplit)
-    # Set initial parameters if do not exist
-    if initial is None:
-        initial = [ff[0], wf[0],
-                   - np.log(abs(f[-1]/f[0])) / np.log(abs(w[-1]/w[0])), 0.0,
-                   900.0, max(ff), 0.0, 1200.0, max(ff)]
-    fem = Fe2V(*initial[3:], bounds={'l1_shift': [-1000.0, 1000.0],
-                                     'l1_i_r': [0.0, 50.0],
-                                     'ne_shift': [-1000.0, 1000.0],
-                                     'n3_i_r': [0.0, 50.0]})
-    cf = ContSdss(*initial[0:3], fixed={'x_0': True}) + fem
-    res = lmlsq(cf, wf, ff, ef, 100000)
-    return res
+    cf = LmCF(wf[0], ff[0], ff[-1], initp=initial)
+    res = conggrad(cf, wf, ff, ef, 10000)
+    report_fit(res)
+    return res.params
 
 
-def hofit(w, f, e, cf, initial):
-    ff = f - cf(w)
-    if initial is None:
-        initial = [10.0, 0.0, 500.0, 40.0, 0.0, 1800.0, 40.0, 0.0, 1800.0,
-                   20.0, 0.0, 500.0, 5008.22, 20.0, 0.0, 500.0, 4960.36,
-                   10.0, 0.0, 1500.0, 4102.73, 20.0, 0.0, 1500.0, 4346.42]
-    hbeta = Hbeta2(*initial[0:9], bounds={'n_a': [0.0, 50.0],
-                                          'w1_a': [0.0, 50.0],
-                                          'w2_a': [0.0, 50.0]})
-    o3 = Narrow(*initial[9:13], fixed={'c': True}, bounds={
-        'a': [0.0, 50.0], 's': [-1000.0, 1000.0], 'w': [0.0, 1200.0]}) + \
-        Narrow(*initial[13:17], fixed={'c': True}, bounds={
-            'a': [0.0, 50.0], 's': [-1000.0, 1000.0], 'w': [0.0, 1200.0]})
-    hdelta = Narrow(*initial[17:21], fixed={'c': True}, bounds={
-        'a': [0.0, 50.0]})
-    hgamma = Narrow(*initial[21:], fixed={'c': True}, bounds={
-        'a': [0.0, 50.0]})
-    all_lines = hbeta + o3 + hdelta + hgamma
-    res = lmlsq(all_lines, w, ff, e, 100000)
-    return res
+def hofit(w, f, e, cf_res, initial):
+    ws, fs, es = splitspec(w, f, e, [[4750, 5100.0]])
+    ff = fs - LmCF(4200.0, 1.0, 2.0).eval(cf_res, ws)
+    ho = LmHO(initp=initial)
+    res = conggrad(ho, ws, ff, es, 10000)
+    report_fit(res)
+    return res.params
 
 
-def spectra_fit(rmid, mjd, isMc, cont_init, line_init, w, f, e):
+def spectra_fit(rmid, mjd, isMc=False, cont_init=None, line_init=None, w=None,
+                f=None, e=None):
     if w is None and f is None and e is None:
         try:
             w, f, e = get_spec("data/calib/pt/" + str(rmid) + "-" + str(mjd) +
                                ".pkl")
+            w, f, e = splitspec(w, f, e, [[4000.0, 5500.0]])
         except Exception:
             logger = logging.getLogger("root")
             logger.error(str(rmid) + " " + str(mjd) + ": spectra not found.")
             return []
     with warnings.catch_warnings():
         warnings.filterwarnings('error')
-        try:
-            cont_res = cffit(w, f, e, cont_init)
-        except Exception as e:
-            print(str(e))
-            logger = logging.getLogger("root")
-            logger.error(str(rmid) + " " + str(mjd) +
-                         ": continuum fitting failure")
-            return []
-        try:
-            line_res = hofit(w, f, e, cont_res, line_init)
-        except Exception:
-            logger = logging.getLogger("root")
-            logger.error(str(rmid) + " " + str(mjd) +
-                         ": line fitting failure")
+        cont_res = cffit(w, f, e, cont_init)
+        line_res = hofit(w, f, e, cont_res, line_init)
         if not isMc:
             save_fit(rmid, mjd, [cont_res, line_res], w, f)
             return []
         else:
-            return [cont_res.parameters, line_res.parameters]
+            return [cont_res, line_res, w, f]
 
 
 def save_fit(rmid, mjd, res_list, w, f):
@@ -98,11 +66,9 @@ def save_fit(rmid, mjd, res_list, w, f):
     f.close()
 
 
+'''
 if __name__ == "__main__":
     logging.config.fileConfig("process_log.conf")
-    f = open(os.path.join(Location.root, "data/source_list.pkl"), "rb")
-    source_list = pickle.load(f)
-    f.close()
     mjd_list = [56660, 56664, 56669, 56683, 56686, 56697, 56713, 56715, 56717,
                 56720, 56722, 56726, 56739, 56745, 56747, 56749, 56751, 56755,
                 56768, 56772, 56780, 56782, 56783, 56795, 56799, 56804, 56808,
@@ -110,3 +76,16 @@ if __name__ == "__main__":
     args = [(each, each_mjd, False, None, None, None, None, None)
             for each in source_list for each_mjd in mjd_list]
     res = para_return(spectra_fit, args, num_thread=100)
+'''
+f = open(os.path.join(Location.root, "data/source_list.pkl"), "rb")
+source_list = pickle.load(f)
+f.close()
+for each in source_list:
+    cf, ho, w, f = spectra_fit(each, 56660, isMc=True)
+    cff = LmCF(4000.0, 1.0, 2.0).eval(cf, w)
+    hof = LmHO().eval(ho, w)
+    plt.plot(w, f)
+    plt.plot(w, cff + hof)
+    plt.plot(w, cff)
+    plt.show()
+    plt.close()
