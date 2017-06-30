0b2e8086c96c0e69268e5650715ea360a4cafc64
chengxinlun <chengxinlun@gmail.com>
2017-06-30 16:15:35 +0200

    Apply new API

--- fe2_vs_o3.py
+++ fe2_vs_o3.py
@@ -11,31 +11,36 @@ def read_lc(rmid, line):
                                  line + ".txt"))
     mjd_list = fd[:, 0]
     flux = fd[:, 1]
-    return [mjd_list, flux]
+    error = fd[:, 2]
+    return [mjd_list, flux, error]
 
 
 # Filter out invalid result
-def filt(mjd_list, flux):
+def filt(mjd_list, flux, error):
     n0i = np.nonzero(flux)
     norm_mjdl = mjd_list[n0i]
     norm_flux = flux[n0i]
+    norm_error = error[n0i]
     if len(norm_flux) < 0.5 * len(mjd_list):
-        return [[], []]
+        return [[], [], []]
     else:
-        return [norm_mjdl, norm_flux]
+        return [norm_mjdl, norm_flux, norm_error]
 
 
 # Intersection
-def inter(mjdl_list, fl_list):
+def inter(mjdl_list, fl_list, err_list):
     mjd_list = mjdl_list[0]
     for each in mjdl_list:
         mjd_list = np.intersect1d(mjd_list, each)
     flux = []
+    error = []
     for each in range(len(mjdl_list)):
         inter_i = np.nonzero(np.in1d(mjdl_list[each], mjd_list))
         flux.append(fl_list[inter_i])
+        error.append(err_list[inter_i])
     flux = np.array(flux)
-    return [mjd_list, flux]
+    error = np.array(error)
+    return [mjd_list, flux, error]
 
 
 # Get hb, o3
@@ -44,11 +49,16 @@ def ave(rmid):
     o3l = filt(*read_lc(rmid, "o3"))
     fel = filt(*read_lc(rmid, "fe2"))
     a_hb = np.mean(hbl[1])
+    e_hb = np.mean(hbl[2])
     a_o3 = np.mean(o3l[1])
+    e_o3 = np.mean(o3l[2])
     a_fe = np.mean(fel[1])
+    e_fe = np.mean(fel[2])
     r_o3 = a_o3 / a_hb
     r_fe = a_fe / a_hb
-    return [r_o3, r_fe]
+    r_o3_e = (a_o3 * e_hb + e_o3 * a_hb) / (a_hb * a_hb)
+    r_fe_e = (a_fe * e_hb + e_fe * a_hb) / (a_hb * a_hb)
+    return [r_o3, r_fe, r_o3_e, r_fe_e]
 
 
 if __name__ == "__main__":
@@ -64,7 +74,8 @@ if __name__ == "__main__":
         except Exception:
             continue
     data_list = np.array(data_list)
-    plt.scatter(data_list[:, 0], data_list[:, 1])
+    plt.errorbar(data_list[:, 0], data_list[:, 1], xerr=data_list[:, 2],
+                 yerr=data_list[:, 3], linestyle='none', color='blue', fmt='o')
     plt.xlim([0.0, 2.5])
     plt.ylim([0.0, 25.0])
     plt.xlabel("Relative OIII")
